780891f856973f703a4d4026f6ff93e88142f75c
Preston Carey <pcarey1213@gmail.com>
2018-12-10 17:29:21 -0500

    changes 12-10

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,43 +14,42 @@ $(document).on("click", "#end", function(){
 var questions = [
     
     {
-        statement:"poop",
         question: "What popular New Orleans DJ/Producer is responsible for the tag-line 'We the Best?'",
         answers: ["P-Diddy", "Mike Will-Made-It", "Metro Boomin","DJ Khaled"],
-        correctAnswers: "DJ Khaled"
+        correctAnswer: "DJ Khaled"
     },
     
     {
         question:"What song featuring Drake did DJ Khaled release in summer 2016?",
         answers: ["For Free", "One Dance", "Blank Space", "Mask Off"],
-        correctAnswers: "For Free"
+        correctAnswer: "For Free"
     },
     
     {
         question: "What was the name of DJ Khaled's last album (released in 2017)?", 
         answers: ["We the Best", "Major Key", "Grateful", "Reputation"],
-        correctAnswers: "Grateful"
+        correctAnswer: "Grateful"
         
     },
     
     {
         question: "What Miami-based rapper did DJ Khaled feature on the song, 'Speedin'?", 
         answers: ["Busta Rhymes", "Rick Ross", "Jay-Z", "Lil' Wayne"],
-        correctAnswers: "Rick Ross"
+        correctAnswer: "Rick Ross"
         
     },
     
     {
         question: "Which of these DJ Khaled songs featured Beyonce and Jay-Z?", 
         answers: ["We Takin' Over", "No Brainer", "All I Do Is Win", "Shining"],
-        correctAnswers: "Shining"
+        correctAnswer: "Shining"
         
     },
     
     {
         question: "Which of these Canadian artists sings the chorus on 'No Brainer?'", 
         answers: ["PartyNextDoor", "The Weeknd", "Justin Bieber", "Celine Dion"],
-        correctAnswers: "Justin Bieber"  
+        correctAnswer: "Justin Bieber"  
     
     }]
 
@@ -73,13 +72,13 @@ var game = {
     
         $("#start").remove()
     
-        console.log(questions[i].question);
         $("#game-window").append("<h2>" + questions[i].question + "</h2>");
-    
-        for (var j=0; j<questions[j].answers.length; j++){
-        $("#game-window").append("<input type = 'radio' name='question-"+i+"'+ value='"+questions[i].answers[j]+"'>"+questions[i].answers[j]) ;  
-        console.log(questions[i].answers[j]);
-        }};
+
+            for (var j=0; j<questions[j].answers.length; j++){
+            $("#game-window").append("<input type = 'radio' name='question-"+i+"'+ value='"+questions[i].answers[j]+"'>"+questions[i].answers[j]) ;  
+            }
+        };
+
         $("#game-window").append('<br><button id="end"> Done </button>');
     
     },
@@ -88,39 +87,40 @@ var game = {
         if($(this).val()==questions[0].correctAnswer){
             game.correct++;}
           else {
-            game.incorrect++
+            game.incorrect++;
           }  
         });
         $.each($('input[name="question-1"]:checked'), function () {
             if($(this).val()==questions[1].correctAnswer){
                 game.correct++;}
               else {
-                game.incorrect++
+                game.incorrect++;
               }  
         });
         $.each($('input[name="question-2"]:checked'), function () {
             if( $(this).val()==questions[2].correctAnswer){
                 game.correct++;}
                 else {
-                game.incorrect++
+                game.incorrect++;
                 }  
         });
         $.each($('input[name="question-3"]:checked'), function () {
             if( $(this).val()==questions[3].correctAnswer){
                 game.correct++;}
                 else {
-                game.incorrect++
+                game.incorrect++;
                 }  
         });
         $.each($('input[name="question-4"]:checked'), function () {
             if( $(this).val()==questions[4].correctAnswer){
                 game.correct++;}
                 else {
-                game.incorrect++
+                game.incorrect++;
                 }  
         });
         $.each($('input[name="question-5"]:checked'), function () {
-            if( $(this).val()==questions[5].correctAnswer){
+            if($(this).val()==questions[5].correctAnswer){
+                console.log
                 game.correct++;}
                 else {
                 game.incorrect++
